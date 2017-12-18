0961de340a16f052b1358b0a2e342d828fed1130
uncommonAP <aaron.paul_r@outlook.com>
2017-12-18 15:55:40 -0500

    component destinations for `Route`s

---
+++ app/javascript/react/components/ContactUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactUs = props => {
+  return(
+    <div>Contact Us</div>
+  )
+}
+
+export default ContactUs
---
+++ app/javascript/react/components/Mission.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mission = props => {
+  return(
+    <div>Mission</div>
+  )
+}
+
+export default Mission
