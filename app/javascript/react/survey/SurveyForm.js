310008aa4c40da46fd2b544e922fe4dbeb24700c
uncommonAP <aaron.paul_r@outlook.com>
2017-12-20 12:54:50 -0500

    create survey landing

---
+++ app/javascript/react/survey/SurveyForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class SurveyForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>Survey</div>
+    )
+  }
+}
+
+export default SurveyForm
