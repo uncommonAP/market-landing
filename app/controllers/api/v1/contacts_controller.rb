fc8002e637c364ea8b901280609f191801efcaf5
uncommonAP <aaron.paul_r@outlook.com>
2017-12-21 11:26:23 -0500

    build contacts controller: routes, methods, and tests

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::ContactsController < ApplicationController
+  def index
+    contacts = Contact.all
+    render json: {contacts: contacts}
+  end
+
+  def create
+    contact = Contact.new(contact_params)
+    if contact.save
+      render json: {contact: contact}
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :updates)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'public_views#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :contacts, only: [:index, :create]
+    end
+  end
+
   get '*path', to: 'public_views#index'
 end
