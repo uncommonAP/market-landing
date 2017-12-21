f27efbe2723e28f53db0e81ce0885bfba9028804
uncommonAP <aaron.paul_r@outlook.com>
2017-12-21 13:20:51 -0500

    build store

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
 
 import LandingPage from '../react/LandingPage'
 
+const middlewares = [thunkMiddleware]
+
+const store = createStore(
+  combineReducers({
+
+  }),
+  applyMiddleware(...middlewares)
+)
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <LandingPage />, document.getElementById('landing')
+    <Provider store={store}><LandingPage /></Provider>, document.getElementById('landing')
   )
 })
