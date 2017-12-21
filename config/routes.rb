5fba4d7a5bdf1c105bd94499547b24edad6de17d
uncommonAP <aaron.paul_r@outlook.com>
2017-12-21 12:49:42 -0500

    routes for question and answer api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :contacts, only: [:index, :create]
+      resources :questions, only: [:index]
+      resources :answers, only: [:create]
     end
   end
 
