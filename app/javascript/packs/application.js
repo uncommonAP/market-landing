056ee52c8cd74096e58bebcd016a7db2e53a5b4d
uncommonAP <aaron.paul_r@outlook.com>
2017-12-22 12:11:08 -0500

    question/answer store

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,12 +5,15 @@ import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 
 import LandingPage from '../react/LandingPage'
+import currentQuestion from '../react/survey/reducers/currentQuestion'
+import answers from '../react/survey/reducers/answers'
 
 const middlewares = [thunkMiddleware]
 
 const store = createStore(
   combineReducers({
-
+    currentQuestion,
+    answers
   }),
   applyMiddleware(...middlewares)
 )
