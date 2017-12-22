2a781c44423e1d5242e49ff0615c8fa6b67682bf
uncommonAP <aaron.paul_r@outlook.com>
2017-12-22 15:51:01 -0500

    add country and region selectors

--- app/javascript/react/survey/SurveyForm.js
+++ app/javascript/react/survey/SurveyForm.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { Switch, Route, Navlink } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { CountryDropdown, RegionDropdown } from 'react-country-region-selector'
 import SurveyQuestions from './SurveyQuestions'
+
+const mapStateToProps = state => {
+  return {
+    
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -12,20 +19,60 @@ const mapDispatchToProps = dispatch => {
 class SurveyFormContainer extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      country: '',
+      region: '',
+      formComplete: false
+    }
+    this.handleCountry = this.handleCountry.bind(this)
+    this.handleRegion = this.handleRegion.bind(this)
+  }
+
+  handleCountry(event) {
+    this.setState({ country: event })
+  }
+
+  handleRegion(event) {
+    this.setState({ region: event, formComplete: true })
   }
 
   render() {
+    let continueButton
+    if (this.state.formComplete) {
+      continueButton = <button>Continue!</button>
+    }
     return(
       <div>
         <Switch>
           <Route strict path={`${this.props.match.path}questions`} component={SurveyQuestions} />
-          <div>SurveyForm</div>
+          <div className='survey'>
+            <h1>Thank you for taking our survey!</h1>
+            <hr />
+            <div>
+              <strong>Please Select Your Country: </strong>
+              <CountryDropdown id='country'
+                defaultOptionLabel='Your Country'
+                value={this.state.country}
+                onChange={this.handleCountry} /><br/>
+            </div>
+            <div>
+              <strong>Please Select Your Region: </strong>
+              <RegionDropdown  id='region'
+                defaultOptionLabel='Your Region'
+                disableWhenEmpty={true}
+                country={this.state.country}
+                value={this.state.region}
+                onChange={this.handleRegion}
+              />
+            </div>
+            {continueButton}
+          </div>
         </Switch>
       </div>
     )
   }
 }
 
-const SurveyForm = connect(null, mapDispatchToProps)(SurveyFormContainer)
+const SurveyForm = connect(mapStateToProps, mapDispatchToProps)(SurveyFormContainer)
 
 export default SurveyFormContainer
