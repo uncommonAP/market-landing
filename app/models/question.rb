e5e216a7abe488e63450e5b583e7bfc0349e29a2
uncommonAP <aaron.paul_r@outlook.com>
2018-01-02 11:54:38 -0500

    active question method

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
   validates_presence_of :question_body
   has_many :answers
   has_many :surveys, through: :answers
+
+  def self.active
+    where(active: true)
+  end
 end
