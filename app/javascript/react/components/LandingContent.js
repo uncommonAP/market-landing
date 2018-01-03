2bb6ce11e6b62315d37bb8d2d1d83b71caa6d946
uncommonAP <aaron.paul_r@outlook.com>
2018-01-03 13:54:58 -0500

    Create landing content

---
+++ app/javascript/react/components/LandingContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingContent = props => {
+  return(
+    <div>
+      <p>Optio provident qui sunt atque voluptate ex. Quaerat animi consequatur sunt. Eum quas repudiandae voluptatem. Voluptatem sed omnis qui. Quia laborum dolorem dolorum at. Vel corrupti quo iure doloremque.</p>
+      <p>Dolores tenetur magni. Aut voluptatem voluptate. Fugiat est molestiae id officiis. Nihil sequi perferendis.</p>
+      <p>Quo minima dolorum vitae. Aspernatur esse minus dignissimos pariatur ipsum. Sed dolores et quia amet est est itaque. Sit nulla aperiam. Sit sint et omnis assumenda ducimus. Maxime quas sapiente ad incidunt nam.</p>
+      <p>Neque est sapiente aperiam maxime nulla id et. Fuga rerum molestiae distinctio earum. Quia qui maxime rerum. Qui qui vel qui. Eos odio a qui suscipit. Quis quia culpa quos assumenda recusandae mollitia. Ab perspiciatis non. Minus possimus ex perferendis quia.</p>
+    </div>
+  )
+}
+
+export default LandingContent
