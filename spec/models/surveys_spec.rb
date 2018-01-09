ce6d772bfd07c6cd4fed728f77b485d49327d078
uncommonAP <aaron.paul_r@outlook.com>
2018-01-09 09:14:12 -0500

    correcting tests to current models

--- spec/models/surveys_spec.rb
+++ spec/models/surveys_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Survey, type: :model do
   let!(:survey) { FactoryBot.create(:survey) }
   let!(:question) { FactoryBot.create(:question) }
-  let!(:answer) { FactoryBot.create(:answer, question_id: question.id, survey_id: survey.id)}
+  let!(:open_answer) { FactoryBot.create(:open_answer, question_id: question.id, survey_id: survey.id)}
 
   describe 'survey object' do
     it "is a Survey" do
@@ -23,12 +23,12 @@ RSpec.describe Survey, type: :model do
 
   describe 'associations' do
     it "has many answers" do
-      expect(survey).to have_many(:answers)
-      expect(answer.survey).to eq(survey)
+      expect(survey).to have_many(:open_answers)
+      expect(open_answer.survey).to eq(survey)
     end
 
     it "has many questions through answers" do
-      expect(survey).to have_many(:questions).through(:answers)
+      expect(survey).to have_many(:questions).through(:open_answers)
       expect(survey.questions).to include(question)
     end
   end
